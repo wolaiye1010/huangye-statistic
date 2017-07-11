8639117162dd4ab7ec1f942bfb7a12522b8b30ba
zhangbo23 <zhangbo23@58ganji.com>
2017-07-11 09:47:31 +0800

    系统耗时web页面

--- statistic-web/src/main/java/com/bj58/statistic/web/controller/StatisticsController.java
+++ statistic-web/src/main/java/com/bj58/statistic/web/controller/StatisticsController.java
@@ -3,12 +3,21 @@ package com.bj58.statistic.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by zhangbo23 on 2017/7/10.
  */
 @Controller
 public class StatisticsController {
+
+    @RequestMapping(value = "/")
+    public ModelAndView index() {
+        return new ModelAndView("forward:/statistic/times?id=times");
+    }
 
     @RequestMapping(value = "/statistic/times")
     public String timeStatistic() {
