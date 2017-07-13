ade8b8f550e09f753811110913fa8dfb2cbb0d54
zhudongchang <zhudongchang@58ganji.com>
2017-07-13 14:14:02 +0800

    web hook

---
+++ statistic-web/src/main/java/com/bj58/statistic/web/controller/WebHookController.java
@@ -0,0 +1,52 @@
+package com.bj58.statistic.web.controller;
+
+
+import com.alibaba.fastjson.JSON;
+import com.bj58.statistic.web.util.CommonUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by zhudongchang on 2017/6/21.
+ */
+@Controller
+public class WebHookController {
+
+    private static final String TOKEN="6661ad53f689a2711d84ab9c3559eae9";
+
+    String shellPath="/opt/web/huangye-statistic/develop.sh";
+
+    @RequestMapping("/api/web_hook")
+    @ResponseBody
+    public void webHook(String token,HttpServletRequest request,HttpServletResponse response) throws IOException, InterruptedException {
+        if(null==token||!token.equals(TOKEN)){
+            return;
+        }
+
+        File file=new File(shellPath);
+        if(!file.exists()){
+            return;
+        }
+
+
+        String requestBody = CommonUtils.getRequestBody(request);
+
+        Map map = JSON.parseObject(requestBody, Map.class);
+
+        if(null==map||!"refs/heads/snapshot".equals(map.get("ref"))){
+            return;
+        }
+
+        response.getWriter().write("---start--");
+        Process exec = Runtime.getRuntime().exec(shellPath);
+        response.getWriter().write("---end--");
+        return;
+    }
+}
